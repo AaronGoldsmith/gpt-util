73badcca10836db532f66899aa50e63deef47aaf
Aaron Goldsmith <agoldsmith@alumni.scu.edu>
2023-05-04 22:58:02 -0700

    Refactor for pylint

--- hooks/gpt_commit_msg.py
+++ hooks/gpt_commit_msg.py
@@ -1,3 +1,8 @@
+"""
+This module is used as a Git hook to generate commit messages using OpenAI GPT-3.5-turbo.
+It takes a diff as input, processes it into chunks, and generates a commit message summary.
+"""
+
 import sys
 import os
 import openai
@@ -10,47 +15,52 @@ def num_tokens_from_string(string: str) -> int:
     num_tokens = len(encoding.encode(string))
     return num_tokens
 
+
 ADDED_CODE = sys.argv[1]
+
+
 def main():
+    """
+    The main function processes the input diff, divides it into chunks,
+    and generates commit messages using the OpenAI API.
+    It then prints the generated commit messages.
+    """
     openai.api_key = os.getenv("OPENAI_API_KEY")
     code_chunks = ADDED_CODE.split('diff --git')
 
     summaries = []
     combined_chunks = []
-    
+
     # Combine code_chunks until the token limit is reached
     current_chunk = ''
     for chunk in code_chunks:
         if chunk:
             temp_chunk = current_chunk + 'diff --git' + chunk
             tokens = num_tokens_from_string(temp_chunk)
-            
+
             if tokens <= 4096:
                 current_chunk = temp_chunk
             else:
                 combined_chunks.append(current_chunk)
                 current_chunk = 'diff --git' + chunk
-    
+
     # Add the last chunk to the combined_chunks list
     if current_chunk:
         combined_chunks.append(current_chunk)
 
-# 2. Identify which category the diff falls into
-#    \n\t [fix][feat][chore][docs][style][refactor][perf][test]   \
-    # print(combined_chunks)
     for chunk in combined_chunks:
         system_intro = "You are a commit message generator used in prepare-commit-msg.\n \
                         1. Review the provided diff\n  \
                         2. Respond with <commit message summarizing +/- changes> \n \
                         3. Return \"Commit Title\nCommit Description (max 120 characters)\" separated by a new line \
-                        Constraint: Respond with the unpunctuated commit message + brief commit description of the changes.\
-        \n"
+                        Constraint: Respond with the unpunctuated commit message +\
+                              brief commit description of the changes.\n"
 
         prompt = f"{chunk}\n<<END_DIFF>>\n\n"
 
-        # Ensure the number of tokens is within the model's limits (4096 tokens for gpt-3.5-turbo)
         if tokens > 4096:
-            print(f"Warning: Skipping file '{code_chunks}' due to token limit exceeded ({tokens} tokens).", file=sys.stderr)
+            print(f"Warning: Skipping file '{code_chunks}' due to token\
+                   limit exceeded ({tokens} tokens).", file=sys.stderr)
             continue
 
         completion = openai.ChatCompletion.create(
@@ -70,6 +80,6 @@ def main():
 
     return generated_commit_message
 
+
 if __name__ == "__main__":
     main()
-
